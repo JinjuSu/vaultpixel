42fbc0196a34319adc865d6811eeb0fd58ba65ad
JinjuSu <jinjuta.s.au@gmail.com>
2024-05-15 23:15:03 +1000

    Starter project

---
+++ src/components/Products.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+    <h1>This is product page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductComp",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
