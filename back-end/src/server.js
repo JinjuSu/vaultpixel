d295155d3ed76f9e7072f27b3c25aba1a1d13ba7
JinjuSu <jinjuta.s.au@gmail.com>
2024-05-20 00:10:03 +1000

    Refractor MongoDB connection session using asycn function start()

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -8,73 +8,67 @@ import {
 let cartItems = cartItemsRaw;
 let products = productsRaw;
 
-async function populatedCartIds(ids) {
+// Connect MongoDB when the server is started
+async function start() {
+  const mongodbURL = `mongodb+srv://vaultpixel-db:769s46jD1a2fLPNK@database1.sqf5wlh.mongodb.net/?retryWrites=true&w=majority&appName=Database1`;
+  const client = new MongoClient(mongodbURL);
+
   await client.connect();
   const db = client.db("vaultpixel-db");
-  return Promise.all(
-    ids.map((id) => db.collection("products").findOne({ id })) //equivalent to using map() in API calling level
-  );
+
+  const app = express();
+  app.use(express.json());
+
+  async function populatedCartIds(ids) {
+    return Promise.all(
+      ids.map((id) => db.collection("products").findOne({ id })) //equivalent to using map() in API calling level
+    );
+  }
+
+  // ------ Read items in the database using app.get() call back
+  app.get("/products", async (req, res) => {
+    const products = await db.collection("products").find({}).toArray();
+    res.send(products);
+  });
+
+  app.get("/users/:userId/cart", async (req, res) => {
+    const user = await db
+      .collection("users")
+      .findOne({ id: req.params.userId });
+    const populatedCart = await populatedCartIds(user.cartItems); // passing cartItems as ids argument
+    res.json(populatedCart);
+  });
+
+  app.get("/product/:productId", async (req, res) => {
+    const productId = req.params.productId;
+    const product = await db.collection("products").findOne({ id: productId });
+    res.json(product);
+  });
+
+  // ------ Add item to cart w/ POST callback function
+  app.post("/cart", (req, res) => {
+    const productId = req.body.id;
+    // push the item into cart directly using productId
+    cartItems.push(productId);
+    const populatedCart = populatedCartIds(cartItems);
+    res.json(populatedCart);
+  });
+
+  // ------ Remove item from cart w/ DELETE callback function
+  app.delete("/cart/:productId", (req, res) => {
+    // need product ID in request params
+    const productId = req.params.productId;
+    console.log(productId);
+    cartItems = cartItems.filter((id) => id.toString() !== productId);
+    console.log(cartItems);
+    const populatedCart = populatedCartIds(cartItems);
+    console.log(populatedCart);
+    res.json(populatedCart);
+  });
+
+  app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
+  });
 }
 
-const mongodbURL = `mongodb+srv://vaultpixel-db:769s46jD1a2fLPNK@database1.sqf5wlh.mongodb.net/?retryWrites=true&w=majority&appName=Database1`;
-const client = new MongoClient(mongodbURL);
-
-const app = express();
-// need this to use .body (for POST request)
-app.use(express.json());
-
-// app.get("/hello", async (req, res) => {
-//   await client.connect();
-//   const db = client.db("vaultpixel-db"); //name of the database as arg
-//   const products = await db.collection("products").find({}).toArray();
-//   res.send(products);
-// });
-
-// ------ Read items in the database using app.get() call back
-app.get("/products", async (req, res) => {
-  await client.connect();
-  const db = client.db("vaultpixel-db"); //name of the database as arg
-  const products = await db.collection("products").find({}).toArray();
-  res.send(products);
-});
-
-app.get("/users/:userId/cart", async (req, res) => {
-  await client.connect();
-  const db = client.db("vaultpixel-db");
-  const user = await db.collection("users").findOne({ id: req.params.userId });
-  const populatedCart = await populatedCartIds(user.cartItems); // passing cartItems as ids argument
-  res.json(populatedCart);
-});
-
-app.get("/product/:productId", async (req, res) => {
-  await client.connect();
-  const db = client.db("vaultpixel-db");
-  const productId = req.params.productId;
-  const product = await db.collection("products").findOne({ id: productId });
-  res.json(product);
-});
-
-// ------ Add item to cart w/ POST callback function
-app.post("/cart", (req, res) => {
-  const productId = req.body.id;
-  // push the item into cart directly using productId
-  cartItems.push(productId);
-  const populatedCart = populatedCartIds(cartItems);
-  res.json(populatedCart);
-});
-
-// ------ Remove item from cart w/ DELETE callback function
-app.delete("/cart/:productId", (req, res) => {
-  // need product ID in request params
-  const productId = req.params.productId;
-  console.log(productId);
-  cartItems = cartItems.filter((id) => id.toString() !== productId);
-  console.log(cartItems);
-  const populatedCart = populatedCartIds(cartItems);
-  console.log(populatedCart);
-  res.json(populatedCart);
-});
-
-app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
-});
+start();
